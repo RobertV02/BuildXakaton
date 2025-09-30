7ee6927b255020ad6c9130d821bf54f2c206ee46
Robert <rob.pap@mail.ru>
2025-10-01 00:18:40 +0300

    add error

--- mybuild/issues/forms.py
+++ mybuild/issues/forms.py
@@ -6,12 +6,12 @@ class RemarkForm(forms.ModelForm):
         model = Remark
         fields = ['category', 'name', 'fixability', 'issue_type', 'description', 'severity', 'resolution_deadline_days']
         widgets = {
-            'category': forms.TextInput(attrs={'class': 'mt-1 block w-full rounded-md border-gray-300'}),
-            'name': forms.TextInput(attrs={'class': 'mt-1 block w-full rounded-md border-gray-300'}),
-            'fixability': forms.Select(attrs={'class': 'mt-1 block w-full rounded-md border-gray-300'}),
-            'issue_type': forms.Select(attrs={'class': 'mt-1 block w-full rounded-md border-gray-300'}),
+            'category': forms.TextInput(attrs={'class': 'mt-1 block w-full rounded-md border-gray-300', 'required': 'required'}),
+            'name': forms.TextInput(attrs={'class': 'mt-1 block w-full rounded-md border-gray-300', 'required': 'required'}),
+            'fixability': forms.Select(attrs={'class': 'mt-1 block w-full rounded-md border-gray-300', 'required': 'required'}),
+            'issue_type': forms.Select(attrs={'class': 'mt-1 block w-full rounded-md border-gray-300', 'required': 'required'}),
             'description': forms.Textarea(attrs={'rows': 4, 'class': 'mt-1 block w-full rounded-md border-gray-300'}),
-            'severity': forms.Select(attrs={'class': 'mt-1 block w-full rounded-md border-gray-300'}),
+            'severity': forms.Select(attrs={'class': 'mt-1 block w-full rounded-md border-gray-300', 'required': 'required'}),
             'resolution_deadline_days': forms.TextInput(attrs={'class': 'mt-1 block w-full rounded-md border-gray-300', 'placeholder': 'Число дней или -'}),
         }
 
